870e8adae53d6cd04941be3c455ef040ab7655ef
Őri Tamás <oeri.tamas@gmail.com>
2021-10-17 00:37:35 +0200

    inputfield component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,23 +1,174 @@
 import React from "react";
 import Page from "../../components/page/page";
 import Button from "../../components/button/button";
+import InputField from "../../components/input-field/inputField";
+import CheckerField from "../../components/checker-field/checkerField";
+import {options} from "jest-runtime/build/cli/args";
 
 const Home = (props) => {
     return (
         <Page title="Bemutató oldal">
+            <h3>Gombok</h3>
             <div>
                 <Button>Default</Button>
                 <hr/>
-                <Button color="blue">Color: blue</Button>
-                <Button color="blue" isHollow={true}>Color: blue, isHollow: true</Button>
+                <Button color="blue">blue</Button>
+                <Button color="blue" isHollow={true}>blue isHollow</Button>
+                <Button color="blue" isLarge={true}>blue isLarge</Button>
+                <Button color="blue" isHollow={true} isLarge={true}>blue isHollow isLarge</Button>
                 <hr/>
-                <Button color="red">Color: red</Button>
-                <Button color="red" isHollow={true}>Color: red, isHollow: true</Button>
+                <Button color="red">red</Button>
+                <Button color="red" isHollow={true}>red isHollow</Button>
+                <Button color="red" isLarge={true}>red isLarge</Button>
+                <Button color="red" isHollow={true} isLarge={true}>red isHollow isLarge</Button>
                 <hr/>
-                <Button color="yellow">Color: yellow</Button>
-                <Button color="yellow" isHollow={true}>Color: yellow, isHollow: true</Button>
+                <Button color="yellow">yellow</Button>
+                <Button color="yellow" isHollow={true}>yellow isHollow</Button>
+                <Button color="yellow" isLarge={true}>yellow isLarge</Button>
+                <Button color="yellow" isHollow={true} isLarge={true}>yellow isHollow isLarge</Button>
                 <hr/>
                 <Button onClick={() => alert("Test alert")}>onClick alert</Button>
+            </div>
+            <h3>Sima inputok</h3>
+            <div>
+                <InputField label="Teszt label"/>
+                <InputField label="Teszt label number" type="number"/>
+                <InputField label="Teszt label password" type="password"/>
+                <InputField label="Teszt label password" type="password"/>
+                <InputField label="Teszt label with error" type="password" reduxFormData={{
+                    input: {},
+                    meta: {
+                        touched: true,
+                        error: "Ez egy error, ajjaj!"
+                    },
+                }}/>
+                <h4>Checkboxes</h4>
+                <CheckerField label="Teszt label normal" options={[
+                    {
+                        label: "Option 1",
+                        value: "option_1"
+                    },
+                    {
+                        label: "Option 2",
+                        value: "option_2"
+                    },
+                    {
+                        label: "Option 3",
+                        value: "option_3"
+                    }
+                ]}/>
+                <CheckerField label="Teszt label red" color="red" options={[
+                    {
+                        label: "Option 1",
+                        value: "option_1"
+                    },
+                    {
+                        label: "Option 2",
+                        value: "option_2"
+                    },
+                    {
+                        label: "Option 3",
+                        value: "option_3"
+                    }
+                ]}/>
+                <CheckerField label="Teszt label red" color="yellow" options={[
+                    {
+                        label: "Option 1",
+                        value: "option_1"
+                    },
+                    {
+                        label: "Option 2",
+                        value: "option_2"
+                    },
+                    {
+                        label: "Option 3",
+                        value: "option_3"
+                    }
+                ]}/>
+                <CheckerField label="Teszt label red" reduxFormData={{
+                    input: {},
+                    meta: {
+                        touched: true,
+                        error: "Ez egy error, ajjaj!"
+                    },
+                }} options={[
+                    {
+                        label: "Option 1",
+                        value: "option_1"
+                    },
+                    {
+                        label: "Option 2",
+                        value: "option_2"
+                    },
+                    {
+                        label: "Option 3",
+                        value: "option_3"
+                    }
+                ]}/>
+                <h4>Radio</h4>
+                <CheckerField type="radio" label="Teszt label normal" options={[
+                    {
+                        label: "Option 1",
+                        value: "option_1"
+                    },
+                    {
+                        label: "Option 2",
+                        value: "option_2"
+                    },
+                    {
+                        label: "Option 3",
+                        value: "option_3"
+                    }
+                ]}/>
+                <CheckerField type="radio" label="Teszt label red" color="red" options={[
+                    {
+                        label: "Option 1",
+                        value: "option_1"
+                    },
+                    {
+                        label: "Option 2",
+                        value: "option_2"
+                    },
+                    {
+                        label: "Option 3",
+                        value: "option_3"
+                    }
+                ]}/>
+                <CheckerField type="radio" label="Teszt label yellow" color="yellow" options={[
+                    {
+                        label: "Option 1",
+                        value: "option_1"
+                    },
+                    {
+                        label: "Option 2",
+                        value: "option_2"
+                    },
+                    {
+                        label: "Option 3",
+                        value: "option_3"
+                    }
+                ]}/>
+                <CheckerField type="radio" label="Teszt label error" reduxFormData={{
+                    input: {},
+                    meta: {
+                        touched: true,
+                        error: "Ez egy error, ajjaj!"
+                    },
+                }} options={[
+                    {
+                        label: "Option 1",
+                        value: "option_1"
+                    },
+                    {
+                        label: "Option 2",
+                        value: "option_2"
+                    },
+                    {
+                        label: "Option 3",
+                        value: "option_3"
+                    }
+                ]}/>
+
 
             </div>
         </Page>
