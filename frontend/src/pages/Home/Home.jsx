8226c3fa916d18022791bafa22ae544bf08df12a
Őri Tamás <oeri.tamas@gmail.com>
2021-10-16 15:48:07 +0200

    test home route and page

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classNames from "classnames";
+import Page from "../../components/page/page";
+
+const Home = (props) => {
+    return (
+        <Page title="Bemutató oldal">
+            <p>Teszt</p>
+        </Page>
+    );
+};
+
+export default Home;
