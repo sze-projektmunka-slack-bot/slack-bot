d7e480a78681c1b132f7d3c8c70a9ad07c64bd8f
Őri Tamás <oeri.tamas@gmail.com>
2021-10-16 14:19:08 +0200

    create folder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router";
+
+const App = () => {
+   return (
+      <div className="app">
+          <Switch>
+              
+          </Switch>
+      </div>
+   );
+};
+
+export default App;
