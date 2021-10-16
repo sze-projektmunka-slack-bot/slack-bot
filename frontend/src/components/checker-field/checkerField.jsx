b6b20cabefea78cbfa41df65529ffc933bbb830a
Őri Tamás <oeri.tamas@gmail.com>
2021-10-17 00:37:46 +0200

    checkerField component

---
+++ frontend/src/components/checker-field/checkerField.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import classNames from "classnames";
+import classes from "./checkerField.module.scss";
+
+const CheckerField = (props) => {
+    //type csak checkbox vagy radio
+    //options felépítés minta:
+    // {
+    //  {
+    //      value: 'test',
+    //      label: 'test'
+    //  },
+    // }
+    const {label, type, className, style, options, reduxFormData, color} = props;
+    const getColorClass = () => {
+        if (color === "red") {
+            return classes.red;
+        }
+        if (color === "blue") {
+            return classes.blue;
+        }
+        if (color === "yellow") {
+            return classes.yellow;
+        }
+        return classes.blue;
+    }
+
+    return (
+        <div className={classNames(classes.inputGroup, reduxFormData && reduxFormData.meta.touched && reduxFormData.meta.error ? classes.inputError : "")}>
+            <label>{label}</label>
+            {reduxFormData && reduxFormData.meta.touched && reduxFormData.meta.error &&
+            <span className={classes.error}>{reduxFormData.meta.error}</span>}
+            {options.map((option) => {
+                return <label className={classes.optionLabel}><input
+                    type={type ?? "checkbox"}
+                    className={
+                        classNames(
+                            className,
+                            classes.input,
+                            getColorClass()
+                        )
+                    }
+                    style={{...(style ? style : {})}}
+                    {...(reduxFormData ? (reduxFormData.input) : {})}
+                    value={option.value}
+                />{option.label}</label>
+            })}
+        </div>
+    );
+};
+
+export default CheckerField;
