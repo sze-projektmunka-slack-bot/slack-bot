01c95cab5ccda0b1afb11d821b7720949adeb7a5
tamastomordi <tomordi.tamas@gmail.com>
2021-11-05 15:15:13 +0100

    Add link to header

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -2,12 +2,15 @@ import classNames from "classnames";
 import classes from "./header.module.scss";
 import logo from '../../assets/logo.svg';
 import Button from "../button/button";
+import { Link } from 'react-router-dom';
 
 const Header = (props) => {
    return (
       <header className={classNames(classes.header)}>
          <div className={classNames(classes.container)}>
-            <img className={classNames(classes.logo)} src={logo} />
+            <Link to="/">
+               <img className={classNames(classes.logo)} src={logo} />
+            </Link>
             <Button 
                className={classNames(classes.logout)}
                isHollow
