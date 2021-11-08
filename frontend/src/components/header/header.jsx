e0ccc8da8644c2c45d64b5477e2f1d332ca767c8
Tamás Tömördi <tomordi.tamas@gmail.com>
2021-11-08 07:23:07 +0100

    Add logout button functionality

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -3,23 +3,37 @@ import classes from "./header.module.scss";
 import logo from '../../assets/logo.svg';
 import Button from "../button/button";
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../../actions';
 
 const Header = (props) => {
+   const onLogoutButtonClick = () => {
+      props.logout();
+   };
+
    return (
       <header className={classNames(classes.header)}>
          <div className={classNames(classes.container)}>
             <Link to="/">
                <img className={classNames(classes.logo)} src={logo} />
             </Link>
-            <Button 
-               className={classNames(classes.logout)}
-               isHollow
-            >
-               Kijelentkezés
-            </Button>
+            {
+               props.isLoggedIn 
+               && 
+               <Button className={classNames(classes.logout)} isHollow
+                  onClick={onLogoutButtonClick}>
+                  Kijelentkezés
+               </Button>
+            }
          </div>
       </header>
    );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+   return {
+      isLoggedIn: state.auth.isLoggedIn
+   };
+};
+
+export default connect(mapStateToProps, {logout})(Header);
