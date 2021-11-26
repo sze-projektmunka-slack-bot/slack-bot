4673733640e3156310ecfbc82141d2b722a52493
Pénzes Péter <penzespeter4@gmail.com>
2021-11-26 15:56:30 +0100

    add button response

---
+++ backend/app/Classes/Responses/Button.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Classes\Responses;
+
+use App\Classes\BaseResponse;
+use App\Classes\ResponseInterface;
+
+class Button extends BaseResponse implements ResponseInterface {
+
+    public static function GetIdentifier(): string {
+        return "response_button";
+    }
+
+    protected string $name = "Gomb";
+
+    protected array $inputs = [
+        [
+            "type"     => "text",
+            "required" => true,
+            "name"     => "response_message",
+            "label"    => "Üzenet",
+        ],
+        [
+            "type"     => "text",
+            "required" => true,
+            "name"     => "button_text",
+            "label"    => "Gomb szövege",
+        ],
+        [
+            "type"     => "text",
+            "required" => true,
+            "name"     => "button_action",
+            "label"    => "Esemény azonosító",
+        ]
+    ];
+
+    protected string $type = "say";
+
+    protected array $validationRules = [
+        "response_message" => ["required", "string"],
+        "button_text"      => ["required", "string"],
+        "button_action"    => ["required", "string"],
+    ];
+
+    public function GetPayload(): string {
+        return '
+        {
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "' . $this->inputValues["response_message"] . '"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "' . $this->inputValues["button_text"] . '",
+					"emoji": true
+				},
+				"value": "click_me_123",
+				"action_id": "' . \Str::slug($this->inputValues["button_action"]) . '"
+			}
+		}';
+    }
+
+    public function GetNotificationText(): string {
+        return $this->inputValues['response_message'];
+    }
+}
